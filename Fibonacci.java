39ac3b2bcf61448b796de65ac0340dc0bc725c9f
Matt Hough <matt@hough.tv>
2018-08-03 17:45:49 +1000

    Should be else if

--- Fibonacci.java
+++ Fibonacci.java
@@ -11,7 +11,7 @@ public class Fibonacci {
   public static int fib(int n) { 
     if (n == 0)
       return 0;
-    if (n == 1)
+    else if (n == 1)
       return 1;
     else
       return fib(n - 1) + fib(n - 2);
